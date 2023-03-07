3f7f7714d09d6e26ae252633029f0c30537b0a2f
Vincent Mussault <v.mussault@free.fr>
2023-03-07 11:38:02 +0100

    new

--- FrontEnd/authentification.js
+++ FrontEnd/authentification.js
@@ -1,13 +1,11 @@
 async function recherchelogin() {
-        const requeteOptions = {
-        method: 'POST',
-        
-    };
-    const response = await fetch('http://localhost:5678/api/users/login', requeteOptions);
-    const data = await response.json();
-    console.log (data);
+    const recupmail = document.getElementById("saisieemail").value;
+    console.log(recupmail);
+    const recupmdp = document.getElementById("saisiemotdepasse").value;
+    console.log(recupmdp);
+    const reponse = await fetch("http://localhost:5678/api/users/login");
+    console.log(reponse);
 }
-
 const lancementverif = document.getElementById("clicbouton");
 lancementverif.addEventListener("click", function() {
     recherchelogin()
