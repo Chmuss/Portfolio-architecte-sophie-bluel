7470f7dfdadc56104af837141fb28a67e6a1ed6d
Vincent Mussault <v.mussault@free.fr>
2023-03-08 14:57:22 +0100

    avec controle du login

--- FrontEnd/authentification.js
+++ FrontEnd/authentification.js
@@ -3,10 +3,21 @@ async function recherchelogin() {
     console.log(recupmail);
     const recupmdp = document.getElementById("saisiemotdepasse").value;
     console.log(recupmdp);
-    const reponse = await fetch("http://localhost:5678/api/users/login");
+    const reponse = await fetch("http://localhost:5678/api/users/login", {
+            method: "POST",
+            headers: {
+                'Accept': 'application/json', 
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                email: recupmail,
+                password: recupmdp
+            })
+        })
     console.log(reponse);
 }
 const lancementverif = document.getElementById("clicbouton");
-lancementverif.addEventListener("click", function() {
+lancementverif.addEventListener("click", function(event) {
+    event.preventDefault();
     recherchelogin()
 })
